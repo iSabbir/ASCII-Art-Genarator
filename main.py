3399881536cf4c80ab7f5e3f68a2db759bd4040e
Sabbir <131774907+iSabbir@users.noreply.github.com>
2023-10-03 21:08:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import pyfiglet
 
 word = input("Enter a word: ")
-ascii_art = pyfiglet.figlet_format(word, font="bubble")
-print(ascii_art)
+
+font_styles = [
+    "banner",
+    "big",
+    "block",
+    "bubble",
+    "digital",
+    "ivrit",
+    "mini",
+    "script",
+    "shadow",
+    "slant",
+    "small",
+    "smscript",
+    "smshadow",
+    "smslant",
+    "standard"
+]
+
+for font_style in font_styles:
+    ascii_art = pyfiglet.figlet_format(word, font=font_style)
+    print(f"\nFont style: {font_style}")
+    print(ascii_art)
